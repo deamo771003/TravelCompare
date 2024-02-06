b49ddc5fe307ae10d9e2a188ff219321bc99443c
JimmyLin <disillusioned2077@gmail.com>
2024-02-06 15:59:01 +0800

    modify: dockerFile modify install cmd

--- init-app.sh
+++ init-app.sh
@@ -3,21 +3,19 @@
 # 檢索秘密值，請替換 "MySecretName" 為您在 AWS Secrets Manager 中設定的秘密名稱
 DB_CREDENTIALS=$(aws secretsmanager get-secret-value --secret-id travelCompareENV --query SecretString --output text)
 
-echo $DB_CREDENTIALS
-
 # 環境變數賦值
 DB_USERNAME=$(echo $DB_CREDENTIALS | jq -r .DB_USERNAME)
 DB_PASSWORD=$(echo $DB_CREDENTIALS | jq -r .DB_PASSWORD)
 DB_HOST=$(echo $DB_CREDENTIALS | jq -r .DB_HOST)
 
 # 進行資料庫連接檢查  
-while ! /opt/mssql-tools/bin/sqlcmd -S $DB_HOST -U $DB_USERNAME -P $DB_PASSWORD -Q "SELECT 1" ; do
+while ! sqlcmd -S $DB_HOST -U $DB_USERNAME -P $DB_PASSWORD -Q "SELECT 1" ; do
   echo "Waiting for database connection..."
   sleep 2
 done
 
 # 檢查Users表是否存在
-table_exists=$(/opt/mssql-tools/bin/sqlcmd -S $DB_HOST -U $DB_USERNAME -P $DB_PASSWORD -d travelCompare -Q "IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Users') SELECT 1 ELSE SELECT 0" -h -1)
+table_exists=$(sqlcmd -S $DB_HOST -U $DB_USERNAME -P $DB_PASSWORD -d travelCompare -Q "IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Users') SELECT 1 ELSE SELECT 0" -h -1)
 
 # 如果表不存在，執行遷移和種子
 if [ "$table_exists" -eq "0" ]; then

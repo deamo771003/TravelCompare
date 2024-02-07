336bee5d4677981309c4cd94ce40db654494ac3f
JimmyLin <disillusioned2077@gmail.com>
2024-02-07 16:23:14 +0800

    modify: docker port and url

--- dist/app.js
+++ dist/app.js
@@ -3,16 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// 部署GCP
-// CI/CD
-// kubernetes
-let url;
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
-    url = 'http://localhost:';
-}
-else {
-    url = 'https://travelcompare-412314.de.r.appspot.com/';
 }
 const express_1 = __importDefault(require("express"));
 const index_1 = require("./db/models/index");
@@ -20,7 +12,6 @@ const routes_1 = __importDefault(require("./routes"));
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const swagger_1 = __importDefault(require("./swagger"));
 const cors_1 = __importDefault(require("cors"));
-// import { getConfig } from './config/config'
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 // Swagger UI
@@ -34,9 +25,5 @@ app.get('/', (req, res) => {
 });
 app.use(routes_1.default);
 app.listen(port, () => {
-    console.log(`express sever is running on ${url}${port}`);
+    console.log(`express sever is running on PORT:${port}`);
 });
-// (async () => {
-//     const config = await getConfig();
-//     console.log(config);
-// })();
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,5 @@
-// 部署GCP
-// CI/CD
-// kubernetes
-let url: string
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
-  url = 'http://localhost:'
-} else {
-  url = 'https://travelcompare-412314.de.r.appspot.com/'
 }
 import express, { Request, Response } from 'express'
 import { initializeDatabase } from './db/models/index'
@@ -14,8 +7,6 @@ import routes from './routes'
 import swaggerUi from 'swagger-ui-express'
 import swaggerSpec from './swagger'
 import cors from 'cors'
-// import { getConfig } from './config/config'
-
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -37,10 +28,5 @@ app.get('/', (req: Request, res: Response) => {
 app.use(routes)
 
 app.listen(port, () => {
-  console.log(`express sever is running on ${url}${port}`)
+  console.log(`express sever is running on PORT:${port}`)
 });
-
-// (async () => {
-//     const config = await getConfig();
-//     console.log(config);
-// })();

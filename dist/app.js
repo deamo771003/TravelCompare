58309a7b40769ac1998729df1d0c6608e8d108ff
JimmyLin <disillusioned2077@gmail.com>
2024-02-18 13:32:17 +0800

    feat: debug about AWS EC2 DB initialize

--- dist/app.js
+++ dist/app.js
@@ -35,7 +35,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config();
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./routes"));
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-require('dotenv').config()
+import dotenv from 'dotenv'
+dotenv.config()
 import express, { Request, Response } from 'express'
 import routes from './routes'
 import swaggerUi from 'swagger-ui-express'

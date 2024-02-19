06f2bdd421d53551c6df12cc278095825dadfdd9
JimmyLin <disillusioned2077@gmail.com>
2024-02-19 10:31:37 +0800

    feat: debug about AWS EC2 DB initialize

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,11 +2,12 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 function getEnvOrSecret(key: string): string {
-  const value = process.env[key];
-  if (value !== undefined) {
-    return value;
-  }
-  throw new Error(`Environment variable ${key} is not defined`)
+  const value = process.env[key]!;
+  // if (value !== undefined) {
+  //   return value;
+  // }
+  // throw new Error(`Environment variable ${key} is not defined`)
+  return value
 }
 
 export const config = {

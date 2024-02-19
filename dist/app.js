3daa057878cb3ef7c460bd353041605d80719d74
JimmyLin <disillusioned2077@gmail.com>
2024-02-19 14:18:42 +0800

    feat: debug about AWS EC2 DB initialize

--- dist/app.js
+++ dist/app.js
@@ -1,74 +1,32 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// import dotenv from 'dotenv'
-// dotenv.config()
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./routes"));
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const swagger_1 = __importDefault(require("./swagger"));
 const cors_1 = __importDefault(require("cors"));
 const loadSecrets_1 = require("./helpers/loadSecrets");
-// import { initializeDatabase } from './db/models/index'
+const index_1 = require("./db/models/index");
 (0, loadSecrets_1.loadSecrets)();
 console.log(`AWS_DB_HOST=${process.env.AWS_DB_HOST}`);
 console.log(`AWS_URL=${process.env.AWS_URL}`);
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
-function startServer() {
-    return __awaiter(this, void 0, void 0, function* () {
-        console.log(`AWS_DB_HOST=${process.env.AWS_DB_HOST}`);
-        console.log(`AWS_URL=${process.env.AWS_URL}`);
-        // Swagger UI
-        app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_1.default));
-        app.use((0, cors_1.default)());
-        const databaseModule = yield Promise.resolve().then(() => __importStar(require('./db/models/index')));
-        yield databaseModule.initializeDatabase();
-        app.use(express_1.default.urlencoded({ extended: true }));
-        app.use(express_1.default.json());
-        app.get('/', (req, res) => {
-            res.send('Hello world!');
-        });
-        app.use(routes_1.default);
-        app.listen(port, () => {
-            console.log(`express sever is running on PORT:${port}`);
-        });
-    });
-}
-startServer().catch(error => {
-    console.error("Failed to start the server:", error);
+// Swagger UI
+app.use('/api-docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_1.default));
+app.use((0, cors_1.default)());
+(0, index_1.initializeDatabase)();
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.json());
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+app.use(routes_1.default);
+app.listen(port, () => {
+    console.log(`express sever is running on PORT:${port}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-// import dotenv from 'dotenv'
-// dotenv.config()
+import dotenv from 'dotenv'
+dotenv.config()
 import express, { Request, Response } from 'express'
 import routes from './routes'
 import swaggerUi from 'swagger-ui-express'
 import swaggerSpec from './swagger'
 import cors from 'cors'
 import { loadSecrets } from './helpers/loadSecrets'
-// import { initializeDatabase } from './db/models/index'
+import { initializeDatabase } from './db/models/index'
 
 loadSecrets()
 console.log(`AWS_DB_HOST=${process.env.AWS_DB_HOST}`)
@@ -14,32 +14,20 @@ console.log(`AWS_URL=${process.env.AWS_URL}`)
 const app = express()
 const port = process.env.PORT || 3000
 
-async function startServer() {
-  
-  console.log(`AWS_DB_HOST=${process.env.AWS_DB_HOST}`)
-  console.log(`AWS_URL=${process.env.AWS_URL}`)
+// Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use(cors())
+initializeDatabase()
 
-  // Swagger UI
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
-  app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-  const databaseModule = await import('./db/models/index');
-  await databaseModule.initializeDatabase()
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world!')
+})
 
-  app.use(express.urlencoded({ extended: true }))
-  app.use(express.json())
+app.use(routes)
 
-  app.get('/', (req: Request, res: Response) => {
-    res.send('Hello world!')
-  })
-
-  app.use(routes)
-
-  app.listen(port, () => {
-    console.log(`express sever is running on PORT:${port}`)
-  });
-}
-
-startServer().catch(error => {
-  console.error("Failed to start the server:", error)
+app.listen(port, () => {
+  console.log(`express sever is running on PORT:${port}`)
 })

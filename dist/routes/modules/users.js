c2747b711714f25e332e801e9c8f9e6d25b0ebec
JimmyLin <disillusioned2077@gmail.com>
2024-02-20 15:21:34 +0800

    feat: debug swagger

--- dist/routes/modules/users.js
+++ dist/routes/modules/users.js
@@ -6,30 +6,30 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 const user_controller_1 = __importDefault(require("../../controllers/user-controller"));
-/**
- * @swagger
- * /users/signup:
- *   post:
- *     summary: Sign up a new user
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - username
- *               - password
- *             properties:
- *               username:
- *                 type: string
- *               password:
- *                 type: string
- *     responses:
- *       200:
- *         description: User successfully signed up
- *       400:
- *         description: Bad request
- */
+// /**
+//  * @swagger
+//  * /users/signup:
+//  *   post:
+//  *     summary: Sign up a new user
+//  *     requestBody:
+//  *       required: true
+//  *       content:
+//  *         application/json:
+//  *           schema:
+//  *             type: object
+//  *             required:
+//  *               - username
+//  *               - password
+//  *             properties:
+//  *               username:
+//  *                 type: string
+//  *               password:
+//  *                 type: string
+//  *     responses:
+//  *       200:
+//  *         description: User successfully signed up
+//  *       400:
+//  *         description: Bad request
+//  */
 router.post('/signup', user_controller_1.default.signup);
 exports.default = router;
--- src/routes/modules/users.ts
+++ src/routes/modules/users.ts
@@ -2,31 +2,31 @@ import express from 'express'
 const router = express.Router()
 import userController from '../../controllers/user-controller'
 
-/**
- * @swagger
- * /users/signup:
- *   post:
- *     summary: Sign up a new user
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - username
- *               - password
- *             properties:
- *               username:
- *                 type: string
- *               password:
- *                 type: string
- *     responses:
- *       200:
- *         description: User successfully signed up
- *       400:
- *         description: Bad request
- */
+// /**
+//  * @swagger
+//  * /users/signup:
+//  *   post:
+//  *     summary: Sign up a new user
+//  *     requestBody:
+//  *       required: true
+//  *       content:
+//  *         application/json:
+//  *           schema:
+//  *             type: object
+//  *             required:
+//  *               - username
+//  *               - password
+//  *             properties:
+//  *               username:
+//  *                 type: string
+//  *               password:
+//  *                 type: string
+//  *     responses:
+//  *       200:
+//  *         description: User successfully signed up
+//  *       400:
+//  *         description: Bad request
+//  */
 router.post('/signup',userController.signup)
 
 export default router
